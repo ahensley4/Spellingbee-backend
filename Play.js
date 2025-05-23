cb788c650121861a4df684127cfdaf6d31d31357
Midori <mogawa@eou.edu>
2025-05-22 17:09:18 -0700

    Update: added word check functions and popup messages

--- Play.js
+++ Play.js
@@ -32,16 +32,16 @@ function generateRandomString(length) {
 
 // call generateRandomString function
 const randomString = generateRandomString(7); 
-console.log("random string: " + randomString);
+console.log("Random string: " + randomString);
 for (let i = 0; i < 7; i++) {
-    console.log("random character: " + randomString[i]);
+    console.log("Random character: " + randomString[i]);
     // retreive the element with id i
     var elem = document.getElementById(i);
     // display the element with id i
     elem.innerHTML = randomString.slice(i, i+1);
 }
 
-let guessWord = '';
+let guessWord = "";
 let inputActive = true;
 
 // function to display current guess word
@@ -52,7 +52,7 @@ function displayString() {
 
 // function to append character to guess word
 function addChar(i) {
-    console.log("selected character: " + document.getElementById(i).innerHTML);
+    console.log("Selected character: " + document.getElementById(i).innerHTML);
     if (inputActive) {
         // keep appending characters until the enter button is clicked
         guessWord += document.getElementById(i).innerHTML;
@@ -62,31 +62,77 @@ function addChar(i) {
 
 // function to delete last character
 function deleteButton() {
-    console.log("Delete button was pressed");
-    if (inputActive && guessWord.length > 1) {
+    console.log("Delete button was clicked");
+    if (inputActive) {
         guessWord = guessWord.slice(0, -1);
         displayString();
     }
 }
 
+let readyToSubmit = false;
+
 //function to submit guess word
 function enterButton() {
     inputActive = false;
     console.log("Enter button was pressed");
-    //send guessed word to REST
+    readyToSubmit = wordCheck();
+    if (readyToSubmit) {
+        console.log("Send the word to the REST API to check");
+    }
+}
+
+function wordCheck() {
+     console.log("Minimum Requirement Check");
+    // if word doesn’t contain at least 4 letters
+    if (guessWord.length < 4) {
+        // popup message
+        popupShort(); 
+    }
+    // if the key word is not used
+    else
+    {
+        for (let i = 0; i < guessWord.length; i++) {
+            if ((guessWord[i]) == document.getElementById(3).innerHTML) { 
+                return true;
+            }
+        }
+        // popup message
+        popupNoKey();
+    }
+    return readyToSubmit;
+}
+
+// 
+function popupShort() {
+    var popup = document.getElementById("tooShort");
+    console.log("word is too short");
+    popup.classList.toggle("show");
+    guessWord = guessWord.slice(0, 0);
+}
+
+function popupNoKey() {
+    var popup = document.getElementById("noKey");
+    console.log("key word is not used");
+    popup.classList.toggle("show");
+    guessWord = guessWord.slice(0, 0);
 }
 
 
+
+
+/*document.onkeydown = keyPressed();
+// from lab5
+// declares global variable first and assigns it true
+var first = true;
+
 // keyPressed() function
-/*document.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        console.log("Guess word: ", guessWord);
-    } else {
-        guessWord += event.key;
-        console.log("Current word: ", guessWord);
+function keyPressed(e) {
+    if (e.code == "Enter") {
+        enterKey();
     }
-})*/
+}
 
-
-
-
+function enterKey() {
+    console.log("Enter key was pressed");
+    //send guessed word to REST
+}*/
